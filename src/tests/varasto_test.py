73a2bd7cb2ac65a251a06614e13ae5ab278238d0
Emil Kivelä <emil.kivela@gmail.com>
2023-11-01 15:51:41 +0200

    Broke varasto testing

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -8,7 +8,7 @@ class TestVarasto(unittest.TestCase):
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
-        self.assertAlmostEqual(self.varasto.saldo, 0)
+        self.assertAlmostEqual(self.varasto.saldo, 2)
     
     def test_varaston_tilavuus_ei_negatiivinen(self):
         varasto = Varasto(-10)
